bbb136820db21e21faae457118f9a1ad5f4c7e96
ajgoward <ajgoward@gmail.com>
2020-08-17 11:07:34 +0000

    started wiring my questions up to javascript and having the total displayed

--- assets/javaScript/quiz.js
+++ assets/javaScript/quiz.js
@@ -1,3 +1,24 @@
 function check(){
-    console.log("sucess")
+   var question1 = document.quiz.question1.value;
+   var question2 = document.quiz.question2.value;
+   var question3 = document.quiz.question3.value;
+   var question4 = document.quiz.question4.value;
+   var total = 0;
+
+   if (question1 == "affection"){
+       total++;
+   }
+   if (question2 == "communicate"){
+       total++;
+   }
+   if (question3 == "Yes"){
+       total++;
+   }
+   if (question4 == "bored"){
+       total++;
+   }
+
+   document.getElementById("after_quiz").style.visibility = "visible";
+   document.getElementById("correct").innerHTML = ("You got" + total + "Correct");
+
 }
